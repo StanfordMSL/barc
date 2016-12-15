911e8e36d01bf387634da1de7ecbe757b0ee61e6
Michael Garstka <michael@garstka.org>
2016-12-15 09:36:04 -0800

    IP settings

--- env_loader_pc.sh
+++ env_loader_pc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
-source /home/max/barc/workspace/devel/setup.bash
-export PATH=$PATH:/home/max/Downloads/julia-2e358ce975/bin
-export ROS_IP=192.168.100.72
-export ROS_MASTER_URI=http://192.168.100.72:11311
+source /home/micha/Documents/barc/workspace/devel/setup.bash
+export PATH=$PATH:/usr/local/bin/julia4
+export ROS_IP=192.168.100.53
+export ROS_MASTER_URI=http://192.168.100.53:11311
 exec "$@"
